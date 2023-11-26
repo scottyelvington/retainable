a14be46e6f6f838fc7b8c3d292562768f11a18d7
Scott Yelvington <scottyelvington@gmail.com>
2023-11-26 15:20:53 -0800

    Adds up to five capture overloads and decomposes them as function parameters in the weakly retained function.

--- Sources/Retainable/Retainable+Strong.swift
+++ Sources/Retainable/Retainable+Strong.swift
@@ -40,6 +40,66 @@ public extension Retainable {
         }
     }
     
+    /// n-2 input to void function
+    func strong<ContextA, ContextB, each Input>(
+        capture: (ContextA, ContextB),
+        _ function: @escaping InputVoidFunctionGetter<ContextA, ContextB, repeat each Input>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputVoidFunction<repeat each Input> {
+        {
+            (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            function(self)(capture.0, capture.1, repeat each item)
+        }
+    }
+    
+    /// n-3 input to void function
+    func strong<ContextA, ContextB, ContextC, each Input>(
+        capture: (ContextA, ContextB, ContextC),
+        _ function: @escaping InputVoidFunctionGetter<ContextA, ContextB, ContextC, repeat each Input>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputVoidFunction<repeat each Input> {
+        {
+            (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            function(self)(capture.0, capture.1, capture.2, repeat each item)
+        }
+    }
+    
+    /// n-4 input to void function
+    func strong<ContextA, ContextB, ContextC, ContextD, each Input>(
+        capture: (ContextA, ContextB, ContextC, ContextD),
+        _ function: @escaping InputVoidFunctionGetter<ContextA, ContextB, ContextC, ContextD, repeat each Input>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputVoidFunction<repeat each Input> {
+        {
+            (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            function(self)(capture.0, capture.1, capture.2, capture.3, repeat each item)
+        }
+    }
+    
+    /// n-5 input to void function
+    func strong<ContextA, ContextB, ContextC, ContextD, ContextE, each Input>(
+        capture: (ContextA, ContextB, ContextC, ContextD, ContextE),
+        _ function: @escaping InputVoidFunctionGetter<ContextA, ContextB, ContextC, ContextD, ContextE, repeat each Input>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputVoidFunction<repeat each Input> {
+        {
+            (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            function(self)(capture.0, capture.1, capture.2, capture.3, capture.4, repeat each item)
+        }
+    }
+    
     /// n-input to output function
     func strong<each Input, Output>(
         _ function: @escaping InputOutputFunctionGetter<repeat each Input, Output>,
@@ -66,6 +126,66 @@ public extension Retainable {
             defer { deferring?() }
             
             return function(self)(capture, repeat each item)
+        }
+    }
+    
+    /// n-2 input to output function
+    func strong<ContextA, ContextB, each Input, Output>(
+        capture: (ContextA, ContextB),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, repeat each Input, Output>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output> {
+        {
+            (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            return function(self)(capture.0, capture.1, repeat each item)
+        }
+    }
+    
+    /// n-3 input to output function
+    func strong<ContextA, ContextB, ContextC, each Input, Output>(
+        capture: (ContextA, ContextB, ContextC),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, ContextC, repeat each Input, Output>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output> {
+        {
+            (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            return function(self)(capture.0, capture.1, capture.2, repeat each item)
+        }
+    }
+    
+    /// n-4 input to output function
+    func strong<ContextA, ContextB, ContextC, ContextD, each Input, Output>(
+        capture: (ContextA, ContextB, ContextC, ContextD),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, ContextC, ContextD, repeat each Input, Output>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output> {
+        {
+            (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            return function(self)(capture.0, capture.1, capture.2, capture.3, repeat each item)
+        }
+    }
+    
+    /// n-5 input to output function
+    func strong<ContextA, ContextB, ContextC, ContextD, ContextE, each Input, Output>(
+        capture: (ContextA, ContextB, ContextC, ContextD, ContextE),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, ContextC, ContextD, ContextE, repeat each Input, Output>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output> {
+        {
+            (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            return function(self)(capture.0, capture.1, capture.2, capture.3, capture.4, repeat each item)
         }
     }
     
--- Sources/Retainable/Retainable+Weak.swift
+++ Sources/Retainable/Retainable+Weak.swift
@@ -44,6 +44,74 @@ public extension Retainable {
         }
     }
     
+    /// n-2 input to void function
+    func weak<ContextA, ContextB, each Input>(
+        capture: (ContextA, ContextB),
+        _ function: @escaping InputVoidFunctionGetter<ContextA, ContextB, repeat each Input>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputVoidFunction<repeat each Input> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return }
+            
+            function(self)(capture.0, capture.1, repeat each item)
+        }
+    }
+    
+    /// n-3 input to void function
+    func weak<ContextA, ContextB, ContextC, each Input>(
+        capture: (ContextA, ContextB, ContextC),
+        _ function: @escaping InputVoidFunctionGetter<ContextA, ContextB, ContextC, repeat each Input>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputVoidFunction<repeat each Input> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return }
+            
+            function(self)(capture.0, capture.1, capture.2, repeat each item)
+        }
+    }
+    
+    /// n-4 input to void function
+    func weak<ContextA, ContextB, ContextC, ContextD, each Input>(
+        capture: (ContextA, ContextB, ContextC, ContextD),
+        _ function: @escaping InputVoidFunctionGetter<ContextA, ContextB, ContextC, ContextD, repeat each Input>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputVoidFunction<repeat each Input> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return }
+            
+            function(self)(capture.0, capture.1, capture.2, capture.3, repeat each item)
+        }
+    }
+    
+    /// n-5 input to void function
+    func weak<ContextA, ContextB, ContextC, ContextD, ContextE, each Input>(
+        capture: (ContextA, ContextB, ContextC, ContextD, ContextE),
+        _ function: @escaping InputVoidFunctionGetter<ContextA, ContextB, ContextC, ContextD, ContextE, repeat each Input>,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputVoidFunction<repeat each Input> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return }
+            
+            function(self)(capture.0, capture.1, capture.2, capture.3, capture.4, repeat each item)
+        }
+    }
+    
     /// n-input to output function
     func weak<each Input, Output>(
         _ function: @escaping InputOutputFunctionGetter<repeat each Input, Output>,
@@ -79,6 +147,78 @@ public extension Retainable {
         }
     }
     
+    /// n-2 input to output function
+    func weak<ContextA, ContextB, each Input, Output>(
+        capture: (ContextA, ContextB),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, repeat each Input, Output>,
+        default defaultValue: Output,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return defaultValue }
+            
+            return function(self)(capture.0, capture.1, repeat each item)
+        }
+    }
+    
+    /// n-3 input to output function
+    func weak<ContextA, ContextB, ContextC, each Input, Output>(
+        capture: (ContextA, ContextB, ContextC),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, ContextC, repeat each Input, Output>,
+        default defaultValue: Output,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return defaultValue }
+            
+            return function(self)(capture.0, capture.1, capture.2, repeat each item)
+        }
+    }
+    
+    /// n-4 input to output function
+    func weak<ContextA, ContextB, ContextC, ContextD, each Input, Output>(
+        capture: (ContextA, ContextB, ContextC, ContextD),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, ContextC, ContextD, repeat each Input, Output>,
+        default defaultValue: Output,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return defaultValue }
+            
+            return function(self)(capture.0, capture.1, capture.2, capture.3, repeat each item)
+        }
+    }
+    
+    /// n-4 input to output function
+    func weak<ContextA, ContextB, ContextC, ContextD, ContextE, each Input, Output>(
+        capture: (ContextA, ContextB, ContextC, ContextD, ContextE),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, ContextC, ContextD, ContextE, repeat each Input, Output>,
+        default defaultValue: Output,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return defaultValue }
+            
+            return function(self)(capture.0, capture.1, capture.2, capture.3, capture.4, repeat each item)
+        }
+    }
+    
     /// n-input to optional output function
     func weak<each Input, Output>(
         _ function: @escaping InputOutputFunctionGetter<repeat each Input, Output>,
@@ -111,6 +251,78 @@ public extension Retainable {
             guard let self else { return defaultValue }
             
             return function(self)(capture, repeat each item)
+        }
+    }
+    
+    /// n-2 input to optional output function
+    func weak<ContextA, ContextB, each Input, Output>(
+        capture: (ContextA, ContextB),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, repeat each Input, Output>,
+        default defaultValue: Output? = nil,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output?> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return defaultValue }
+            
+            return function(self)(capture.0, capture.1, repeat each item)
+        }
+    }
+    
+    /// n-3 input to optional output function
+    func weak<ContextA, ContextB, ContextC, each Input, Output>(
+        capture: (ContextA, ContextB, ContextC),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, ContextC, repeat each Input, Output>,
+        default defaultValue: Output? = nil,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output?> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return defaultValue }
+            
+            return function(self)(capture.0, capture.1, capture.2, repeat each item)
+        }
+    }
+    
+    /// n-4 input to optional output function
+    func weak<ContextA, ContextB, ContextC, ContextD, each Input, Output>(
+        capture: (ContextA, ContextB, ContextC, ContextD),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, ContextC, ContextD, repeat each Input, Output>,
+        default defaultValue: Output? = nil,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output?> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return defaultValue }
+            
+            return function(self)(capture.0, capture.1, capture.2, capture.3, repeat each item)
+        }
+    }
+    
+    /// n-4 input to optional output function
+    func weak<ContextA, ContextB, ContextC, ContextD, ContextE, each Input, Output>(
+        capture: (ContextA, ContextB, ContextC, ContextD, ContextE),
+        _ function: @escaping InputOutputFunctionGetter<ContextA, ContextB, ContextC, ContextD, ContextE, repeat each Input, Output>,
+        default defaultValue: Output? = nil,
+        defer deferring: VoidVoidFunction? = nil
+    ) -> InputOutputFunction<repeat each Input, Output?> {
+        {
+            [weak self] (item: repeat each Input) in
+            
+            defer { deferring?() }
+            
+            guard let self else { return defaultValue }
+            
+            return function(self)(capture.0, capture.1, capture.2, capture.3, capture.4, repeat each item)
         }
     }
     
